1500bcef549b474ecaa4007e25f6cfb87869d7f4
EtiennevanderWalt <etienne.van.der.walt@gmail.com>
2014-10-25 11:14:58 +1100

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -33,6 +33,7 @@ static const int GRID_COLUMNS = 10;
 
 - (void)setupGrid
 {
+    CCLOG(@"blah");
     // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
     _cellWidth = self.contentSize.width / GRID_COLUMNS;
     _cellHeight = self.contentSize.height / GRID_ROWS;
